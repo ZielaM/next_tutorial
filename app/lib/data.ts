03d3af204134a061f1e9af42ed7cc55477b74fdc
ZielaM <izielinski379@gmail.com>
2026-02-26 23:33:27 +0100

    made the dashboard component actually dynamic

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,3 +1,4 @@
+import { unstable_noStore as UnS } from "next/cache";
 import postgres from "postgres";
 import {
   CustomerField,
@@ -12,6 +13,7 @@ import { formatCurrency } from "./utils";
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
 
 export async function fetchRevenue() {
+  UnS();
   try {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
@@ -31,6 +33,7 @@ export async function fetchRevenue() {
 }
 
 export async function fetchLatestInvoices() {
+  UnS();
   try {
     const data = await sql<LatestInvoiceRaw[]>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
@@ -51,6 +54,7 @@ export async function fetchLatestInvoices() {
 }
 
 export async function fetchCardData() {
+  UnS();
   try {
     // You can probably combine these into a single SQL query
     // However, we are intentionally splitting them to demonstrate
@@ -90,6 +94,7 @@ export async function fetchFilteredInvoices(
   query: string,
   currentPage: number,
 ) {
+  UnS();
   const offset = (currentPage - 1) * ITEMS_PER_PAGE;
 
   try {
@@ -122,6 +127,7 @@ export async function fetchFilteredInvoices(
 }
 
 export async function fetchInvoicesPages(query: string) {
+  UnS();
   try {
     const data = await sql`SELECT COUNT(*)
     FROM invoices
@@ -143,6 +149,7 @@ export async function fetchInvoicesPages(query: string) {
 }
 
 export async function fetchInvoiceById(id: string) {
+  UnS();
   try {
     const data = await sql<InvoiceForm[]>`
       SELECT
@@ -168,6 +175,7 @@ export async function fetchInvoiceById(id: string) {
 }
 
 export async function fetchCustomers() {
+  UnS();
   try {
     const customers = await sql<CustomerField[]>`
       SELECT
@@ -185,6 +193,7 @@ export async function fetchCustomers() {
 }
 
 export async function fetchFilteredCustomers(query: string) {
+  UnS();
   try {
     const data = await sql<CustomersTableType[]>`
 		SELECT
